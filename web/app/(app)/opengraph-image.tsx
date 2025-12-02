7e638b9bf00cff194450e5b38dde3632de1c3689
pattreeya <pattreeya@gmail.com>
2025-12-02 02:23:22 +0100

    fix prettier

--- web/app/(app)/opengraph-image.tsx
+++ web/app/(app)/opengraph-image.tsx
@@ -139,87 +139,89 @@ export default async function Image() {
   const logoSize = scaleImageSize(logoDimensions, 24);
 
   return new ImageResponse(
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: size.width,
-        height: size.height,
-        backgroundImage: `url(${bgSrcBase64})`,
-        backgroundSize: '100% 100%',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      {/* wordmark */}
+    (
       <div
         style={{
-          position: 'absolute',
-          top: 30,
-          left: 30,
           display: 'flex',
           alignItems: 'center',
-          gap: 10,
+          justifyContent: 'center',
+          width: size.width,
+          height: size.height,
+          backgroundImage: `url(${bgSrcBase64})`,
+          backgroundSize: '100% 100%',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        <img src={wordmarkSrcBase64} width={wordmarkSize.width} height={wordmarkSize.height} />
-      </div>
-      {/* logo */}
-      <div
-        style={{
-          position: 'absolute',
-          top: 200,
-          left: 460,
-          display: 'flex',
-          alignItems: 'center',
-          gap: 10,
-        }}
-      >
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        <img src={logoSrcBase64} width={logoSize.width} height={logoSize.height} />
-      </div>
-      {/* title */}
-      <div
-        style={{
-          position: 'absolute',
-          bottom: 100,
-          left: 30,
-          width: '380px',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 16,
-        }}
-      >
+        {/* wordmark */}
         <div
           style={{
-            backgroundColor: '#1F1F1F',
-            padding: '2px 8px',
-            borderRadius: 4,
-            width: 72,
-            fontSize: 12,
-            fontFamily: 'CommitMono',
-            fontWeight: 600,
-            color: '#999999',
-            letterSpacing: 0.8,
+            position: 'absolute',
+            top: 30,
+            left: 30,
+            display: 'flex',
+            alignItems: 'center',
+            gap: 10,
           }}
         >
-          SANDBOX
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
+          <img src={wordmarkSrcBase64} width={wordmarkSize.width} height={wordmarkSize.height} />
         </div>
+        {/* logo */}
         <div
           style={{
-            fontSize: 48,
-            fontWeight: 300,
-            fontFamily: 'Everett',
-            color: 'white',
-            lineHeight: 1,
+            position: 'absolute',
+            top: 200,
+            left: 460,
+            display: 'flex',
+            alignItems: 'center',
+            gap: 10,
           }}
         >
-          {pageTitle}
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
+          <img src={logoSrcBase64} width={logoSize.width} height={logoSize.height} />
+        </div>
+        {/* title */}
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 100,
+            left: 30,
+            width: '380px',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 16,
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: '#1F1F1F',
+              padding: '2px 8px',
+              borderRadius: 4,
+              width: 72,
+              fontSize: 12,
+              fontFamily: 'CommitMono',
+              fontWeight: 600,
+              color: '#999999',
+              letterSpacing: 0.8,
+            }}
+          >
+            SANDBOX
+          </div>
+          <div
+            style={{
+              fontSize: 48,
+              fontWeight: 300,
+              fontFamily: 'Everett',
+              color: 'white',
+              lineHeight: 1,
+            }}
+          >
+            {pageTitle}
+          </div>
         </div>
       </div>
-    </div>,
+    ),
     {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
